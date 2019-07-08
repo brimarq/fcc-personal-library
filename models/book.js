6da5d159bb055910faa20c1837200d67f5b959e2
Brian Marquis <rbmarquis.dev@gmail.com>
2019-07-08 16:39:36 -0500

    Create book model

---
+++ models/book.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+var BookSchema = new Schema(
+  {
+    title: {type: String, required: true},
+    comments: [{type: String}]
+  }
+);
+
+BookSchema
+.virtual('commentcount')
+.get(function () {
+  return this.comments.length;
+});
+
+//Export model
+module.exports = mongoose.model('Book', BookSchema);
