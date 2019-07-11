d64f038bea62559abdbc574b66fa6b67be77df29
Brian Marquis <rbmarquis.dev@gmail.com>
2019-07-10 21:14:21 -0500

    Remove virtual from BookSchema

--- models/book.js
+++ models/book.js
@@ -4,15 +4,9 @@ const Schema = mongoose.Schema;
 const BookSchema = new Schema(
   {
     title: {type: String, required: true},
-    comments: [{type: String}]
-  }
+    comments: [String]
+  },
 );
-
-BookSchema
-.virtual('commentcount')
-.get(function () {
-  return this.comments.length;
-});
 
 //Export model
 module.exports = mongoose.model('Book', BookSchema);
