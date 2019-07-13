9a227c73ef86fdbbc9fbbabcbaa9982de22d45a0
Brian Marquis <rbmarquis.dev@gmail.com>
2019-07-12 19:50:27 -0500

    Disable useFindAndModify in mongoose connect method

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 //Set up mongoose connection
 const mongoose = require('mongoose');
 const mongoDB = process.env.MONGODB_URI;
-mongoose.connect(mongoDB, { useNewUrlParser: true });
+mongoose.connect(mongoDB, { useNewUrlParser: true, useFindAndModify: false });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
