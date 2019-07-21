925a9050da19033ae8ff7f2dd0752d35ef4f6b8d
Brian Marquis <rbmarquis.dev@gmail.com>
2019-07-20 21:39:47 -0500

    Finish controller

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,20 +1,22 @@
 const Book = require('../models/book');
+ 
+function createBookObj(book, inclComments = true) { 
+  const bookObj = inclComments 
+    ? (({_id, title, comments} = book) => ({_id, title, comments}))()
+    : (({_id, title, comments} = book) => ({_id, title, commentcount: comments.length}))();
+  return bookObj;
+}
 
-// for route 'api/books'
+// FOR ROUTE 'api/books' //
 
 function getAllBooks(req, res) {
 
-  // I can get /api/books to retrieve an aray of all books containing title, _id, & commentcount. 
-  // response will be array of book objects
-  // json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
+  // I can get /api/books to retrieve an aray of all books containing title, _id, & commentcount. Response will be array of book objects with json format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
 
-  Book.find().lean().sort('comments').exec((err, books) => {
-    if (err) res.send(err);
-    const bookList = books.map(book => {
-      const {_id, title, comments } = book;
-      return { _id, title, commentcount: comments.length };
-    });
-    res.json(bookList);
+  Book.find({}, 'title comments', {lean: true }, (err, books) => {
+    if (err) { throw err }
+    res.send(books.map(book => createBookObj(book, false))
+    );
   });
 }
 
@@ -23,84 +25,83 @@ function addOneBook(req, res) {
   // I can post a title to /api/books to add a book and returned will be the object with the title and a unique _id. 
   // response will contain new book object including at least _id and title 
 
-  const book = new Book({ title: req.body.title });
-  book.save((err, book) => {
+  const newBook = new Book({ title: req.body.title });
+  newBook.save((err, book) => {
     if (err) {
       if (err.name === "ValidationError") {
-        return res.status(400).type('text').send('missing title');
-      }
-      return res.send(err);
-    }
-    const { _id, title, comments } = book;
-    res.json({ _id, title, comments });
+        res.send('missing title');
+      } else { throw err }
+    } else if (book) {
+      res.send(createBookObj(book));
+    } 
   });
 }
 
 function deleteAllBooks(req, res) {
 
-  // I can send a delete request to /api/books to delete all books in the database. Returned will be 'complete delete successful' if successful. 
-  // if successful response will be 'complete delete successful' 
+  // I can send a delete request to /api/books to delete all books in the database. If successful response will be 'complete delete successful' 
 
-  Book.deleteMany(function (err) {
-    if (err) return res.send(err);
-    res.status(200).type('text').send('complete delete successful');
-  });
+  Book.deleteMany(err => {
+    if (err) { throw err }
+    res.send('complete delete successful')
+  })
 }
 
 
-// for route 'api/books/{_id}'
+// FOR ROUTE 'api/books/{_id}' //
 
 function getOneBook(req, res) {
 
-  // I can get /api/books/{_id} to retrieve a single object of a book containing title, _id, & an array of comments (empty array if no comments present). 
-  // json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]} 
-
-  Book.findById(req.params.id, 'title comments', function (err, doc) {
+  // I can get /api/books/{_id} to retrieve a single object of a book containing title, _id, & an array of comments (empty array if no comments present). Json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]} 
+  
+  Book.findById(req.params.id, 'title comments', { lean: true }, (err, book) => {
     if (err) {
-      if (err.name === "CastError" && err.path === "_id") {
-        return res.status(400).type('text').send('no book exists');
-      }
-      return res.send(err);
+      if (err.name === "CastError") {
+        res.send('no book exists');
+      } else { throw err }
     }
-    const {_id, title, comments} = doc;
-    res.json({_id, title, comments});
+    if (book) {
+      res.send(createBookObj(book));
+    }
   });
+
 }
 
 function addComment(req, res) {
 
-  // I can post a comment to /api/books/{_id} to add a comment to a book and returned will be the books object similar to get /api/books/{_id}. 
-  // json res format same as .get 
+  // I can post a comment to /api/books/{_id} to add a comment to a book and returned will be the books object similar to get /api/books/{_id}. Json res format same as .get 
 
   const bookid = req.params.id;
   const comment = req.body.comment;
-  Book.findOneAndUpdate({_id: {$eq: bookid}}, {$push: {comments: comment}}, {new: true}, (err, doc) => {
+
+  Book.findOneAndUpdate({_id: {$eq: bookid}}, {$push: {comments: comment}}, {new: true}, (err, book) => {
     if (err) {
-      if (err.name === "CastError" && err.path === "_id") {
-        return res.status(400).type('text').send('no book exists');
-      }
-      return res.send(err);
+      if (err.name === "CastError") {
+        res.send('no book exists');
+      } else { throw err }
     }
-    const {_id, title, comments} = doc;
-    res.json({_id, title, comments});
+    if (book) {
+      res.send(createBookObj(book));
+    }
   });
+
 }
 
 function deleteOneBook(req, res) {
 
-  // I can delete /api/books/{_id} to delete a book from the collection. Returned will be 'delete successful' if successful. 
-  // if successful response will be 'delete successful' 
+  // I can delete /api/books/{_id} to delete a book from the collection. If successful, response will be 'delete successful' 
 
-  Book.findOneAndDelete({_id: {$eq: req.params.id}}, function (err, doc) {
+  Book.findOneAndDelete({_id: {$eq: req.params.id}}, (err, book) => {
     if (err) {
-      if (err.name === "CastError" && err.path === "_id") {
-        return res.status(400).type('text').send('no book exists');
-      }
-      return res.send(err);
+      if (err.name === "CastError") {
+        res.send('no book exists');
+      } else { throw err }
     }
-    res.status(200).type('text').send('delete successful');
+    if (book) {
+      res.send('delete successful');
+    }
   });
+  
 }
-
 
 module.exports = { getAllBooks, addOneBook, deleteAllBooks, getOneBook, addComment, deleteOneBook};
